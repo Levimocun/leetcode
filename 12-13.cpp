905090d6ba7920d0806decc196790a5d5b455189
levimocun <lvtaohust2014@163.com>
2017-04-12 10:05:41 +0800

    roman and int

--- 12-13.cpp
+++ 12-13.cpp
@@ -6,9 +6,11 @@ Integer to Roman 阿拉伯数字转换为罗马数字
 X  XI  XII  XIII  XIV   XV   XVI  XVII   XVIII   XIX
 20  30    40   50   60    70    80     90   100    110
 XX  XXX   XL    L   LX    LXX   LXXX   XC    C      CX
+
 (1)阿拉伯数字转换为罗马数字思路：
 将1-9，10，20，30，40，50，60，70，80，90，100，200，300，400，500，600，700，800，900，1000列出来，
 每个数字将其转化为a*1000+b*100+c*10+d,然后将a,b,c,d按照表转化，这样拼接起来就是罗马数字。 
+
 (2)罗马数字转换为阿拉伯数字思路：
 从左到右依次读取罗马数字，如果其比前一个数字小，则加上，反之加上并且减去前一个数字的二倍
 https://leetcode.com/problems/roman-to-integer/?tab=Description#/description 
